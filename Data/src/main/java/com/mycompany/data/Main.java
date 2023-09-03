aff8c11f337855e4a20196a5a76db21d596f91f1
Marcio <49918509+MarcioMel0@users.noreply.github.com>
2023-09-03 20:51:05 -0300

    Add files via upload

---
+++ Data/src/main/java/com/mycompany/data/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.data;
+
+/**
+ *
+ * @author Marcio Melo
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Data data = new Data(27,2,2016);
+		data.validaData();
+		data.mostrarData();
+    }
+}
